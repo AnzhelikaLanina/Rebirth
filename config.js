8493b8eb64bc68b4b3b622d8c2e18c4fffa79152
AnzhelikaLanina <anzhelika.sidovi4eva@yandex.ru>
2025-11-24 21:41:23 +0300

    Updates

---
+++ config.js
@@ -0,0 +1,7 @@
+window.__REBIRTH_CONFIG__ = {
+  downloadLinks: {
+    gameClient: { google: "", yandex: "" },
+    gamePatch:  { mediafire: "", yandex: "" },
+    updater:    { google: "", yandex: "" },
+  },
+};
